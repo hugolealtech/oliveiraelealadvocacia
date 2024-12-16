3c4d85d41e62da194f7085487efa0229cb141494
hugolealtech <76668742+hugolealtech@users.noreply.github.com>
2024-12-16 10:20:58 -0300

    estilizaçao de telas de login e criação de component caroussel

---
+++ src/app/components/caroussel/caroussel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-caroussel',
+  imports: [],
+  templateUrl: './caroussel.component.html',
+  styleUrl: './caroussel.component.css'
+})
+export class CarousselComponent {
+
+}
